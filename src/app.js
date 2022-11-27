5c77737abc831bba5dbdf9cb0053658ec07bbf69
Dhaval Chheda <dhavalchheda03@gmail.com>
2022-11-27 13:28:41 +0530

    test

--- src/app.js
+++ src/app.js
@@ -5,21 +5,24 @@ const port=5000;
 
 app.use(express.static('./src/public'));
 
+// app.get('/', (req, res)=>{
+//    res.status(200).sendFile(path.resolve(__dirname,`./public/landing_page.html`))
+// });
+
+// app.get('/newsfeed', (req, res)=>{
+//    res.status(200).sendFile(path.resolve(__dirname,`./public/newsfeed.html`))
+// });
+
+// app.get('/searchnews', (req, res)=>{
+//    res.status(200).sendFile(path.resolve(__dirname,`./public/search.html`))
+// });
+
+// app.all('*',(req,res)=>{
+//    res.status(404).send("resource not found");
+// })
 app.get('/', (req, res)=>{
-   res.status(200).sendFile(path.resolve(__dirname,`./public/landing_page.html`))
+   res.status(200).send("Success")
 });
-
-app.get('/newsfeed', (req, res)=>{
-   res.status(200).sendFile(path.resolve(__dirname,`./public/newsfeed.html`))
-});
-
-app.get('/searchnews', (req, res)=>{
-   res.status(200).sendFile(path.resolve(__dirname,`./public/search.html`))
-});
-
-app.all('*',(req,res)=>{
-   res.status(404).send("resource not found");
-})
 
 module.exports =app.listen(port,()=>{
    console.log(`server listening on port ${port}...`);
--- src/test.js
+++ src/test.js
@@ -10,76 +10,62 @@ const api_url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${API_KE
 
 chai.should();
 chai.use(chaiHttp);
-//chai.use(require('chai-dom'));
-//require('jsdom-global')();
 
-// describe('search.html', () => {
-//     beforeEach((done) => {
-//      JSDOM.fromFile('./public/search.html')
-//      .then((dom) => {
-//        global.document = dom.window.document
-//      })
-//    .then(done, done);
-//    });
+// describe("Get/LandingPage",()=>{
 
-//    describe("Level 1 heading", () => {
-//     it("h1 element should say 'Hello World!'", (done) => {
-//      let element = document.getElementById("Business");
-//      element.click();
-//      activelink(element);
-//      expect(element).to.have.class("active");
-//     //  var [articles,response]=getNews;
-//     //   response.should.have.status(200);
-//      done();
-//     //  expect(articles).to.have.text("Hello World!")
-//     })
-//    })
-
+//     it("it should display landing page",(done)=>{
+//         chai.request(server)
+//             .get("/")
+//             .end((err,res)=>{
+//                 res.should.have.status(200);
+//                 done();
+//             });
+//     });
 // });
 
-describe("Get/LandingPage",()=>{
+// describe("Get/Newsfeed",()=>{
 
-    it("it should display landing page",(done)=>{
+//     it("it should display newsfeed",(done)=>{
+//         chai.request(server)
+//             .get("/newsfeed")
+//             .end((err,res)=>{
+//                 res.should.have.status(200);
+//                 done();
+//             });
+//     });
+// });
+
+// describe("Get/SearchNewsPage",()=>{
+
+//     it("it should display search news page",(done)=>{
+//         chai.request(server)
+//             .get("/searchnews")
+//             .end((err,res)=>{
+//                 res.should.have.status(200);
+//                 done();
+//             });
+//     });
+// });
+
+// describe("Get/anyOtherPage",()=>{
+
+//     it("it should display error message: resource not found",(done)=>{
+//         chai.request(server)
+//             .get("/anyotherpage")
+//             .end((err,res)=>{
+//                 res.should.have.status(404);
+//                 done();
+//             });
+//     });
+// });
+
+describe("Get /",()=>{
+
+    it("it should be successfull",(done)=>{
         chai.request(server)
             .get("/")
             .end((err,res)=>{
                 res.should.have.status(200);
-                done();
-            });
-    });
-});
-
-describe("Get/Newsfeed",()=>{
-
-    it("it should display newsfeed",(done)=>{
-        chai.request(server)
-            .get("/newsfeed")
-            .end((err,res)=>{
-                res.should.have.status(200);
-                done();
-            });
-    });
-});
-
-describe("Get/SearchNewsPage",()=>{
-
-    it("it should display search news page",(done)=>{
-        chai.request(server)
-            .get("/searchnews")
-            .end((err,res)=>{
-                res.should.have.status(200);
-                done();
-            });
-    });
-});
-
-describe("Get/anyOtherPage",()=>{
-
-    it("it should display error message: resource not found",(done)=>{
-        chai.request(server)
-            .get("/anyotherpage")
-            .end((err,res)=>{
-                res.should.have.status(404);
                 done();
             });
     });
