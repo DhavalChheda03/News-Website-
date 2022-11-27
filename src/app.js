8165a5b61a0e37afd3621957f49cbf13626ccb60
Dhaval Chheda <dhavalchheda03@gmail.com>
2022-11-27 15:31:32 +0530

    test11

--- src/app.js
+++ src/app.js
@@ -5,26 +5,26 @@ const path=require("path");
 const port=process.env.PORT;
 
 
-// app.use(express.static('./src/public'));
+app.use(express.static('./src/public'));
 
-// app.get('/', (req, res)=>{
-//    res.status(200).sendFile(path.resolve(__dirname,`./public/landing_page.html`))
-// });
-
-// app.get('/newsfeed', (req, res)=>{
-//    res.status(200).sendFile(path.resolve(__dirname,`./public/newsfeed.html`))
-// });
-
-// app.get('/searchnews', (req, res)=>{
-//    res.status(200).sendFile(path.resolve(__dirname,`./public/search.html`))
-// });
-
-// app.all('*',(req,res)=>{
-//    res.status(404).send("resource not found");
-// })
 app.get('/', (req, res)=>{
-   res.status(200).send("Success")
+   res.status(200).sendFile(path.resolve(__dirname,`./public/landing_page.html`))
 });
+
+app.get('/newsfeed', (req, res)=>{
+   res.status(200).sendFile(path.resolve(__dirname,`./public/newsfeed.html`))
+});
+
+app.get('/searchnews', (req, res)=>{
+   res.status(200).sendFile(path.resolve(__dirname,`./public/search.html`))
+});
+
+app.all('*',(req,res)=>{
+   res.status(404).send("resource not found");
+})
+// app.get('/', (req, res)=>{
+//    res.status(200).send("Success")
+// });
 
 module.exports =app.listen(port,()=>{
    console.log(`server listening on port ${port}...`);
--- src/test.js
+++ src/test.js
@@ -6,57 +6,9 @@ const { listeners } = require("./app");
 chai.should();
 chai.use(chaiHttp);
 
-// describe("Get/LandingPage",()=>{
+describe("Get/LandingPage",()=>{
 
-//     it("it should display landing page",(done)=>{
-//         chai.request(server)
-//             .get("/")
-//             .end((err,res)=>{
-//                 res.should.have.status(200);
-//                 done();
-//             });
-//     });
-// });
-
-// describe("Get/Newsfeed",()=>{
-
-//     it("it should display newsfeed",(done)=>{
-//         chai.request(server)
-//             .get("/newsfeed")
-//             .end((err,res)=>{
-//                 res.should.have.status(200);
-//                 done();
-//             });
-//     });
-// });
-
-// describe("Get/SearchNewsPage",()=>{
-
-//     it("it should display search news page",(done)=>{
-//         chai.request(server)
-//             .get("/searchnews")
-//             .end((err,res)=>{
-//                 res.should.have.status(200);
-//                 done();
-//             });
-//     });
-// });
-
-// describe("Get/anyOtherPage",()=>{
-
-//     it("it should display error message: resource not found",(done)=>{
-//         chai.request(server)
-//             .get("/anyotherpage")
-//             .end((err,res)=>{
-//                 res.should.have.status(404);
-//                 done();
-//             });
-//     });
-// });
-
-describe("Get /",()=>{
-
-    it("it should be successfull",(done)=>{
+    it("it should display landing page",(done)=>{
         chai.request(server)
             .get("/")
             .end((err,res)=>{
@@ -65,3 +17,51 @@ describe("Get /",()=>{
             });
     });
 });
+
+describe("Get/Newsfeed",()=>{
+
+    it("it should display newsfeed",(done)=>{
+        chai.request(server)
+            .get("/newsfeed")
+            .end((err,res)=>{
+                res.should.have.status(200);
+                done();
+            });
+    });
+});
+
+describe("Get/SearchNewsPage",()=>{
+
+    it("it should display search news page",(done)=>{
+        chai.request(server)
+            .get("/searchnews")
+            .end((err,res)=>{
+                res.should.have.status(200);
+                done();
+            });
+    });
+});
+
+describe("Get/anyOtherPage",()=>{
+
+    it("it should display error message: resource not found",(done)=>{
+        chai.request(server)
+            .get("/anyotherpage")
+            .end((err,res)=>{
+                res.should.have.status(404);
+                done();
+            });
+    });
+});
+
+// describe("Get /",()=>{
+
+//     it("it should be successfull",(done)=>{
+//         chai.request(server)
+//             .get("/")
+//             .end((err,res)=>{
+//                 res.should.have.status(200);
+//                 done();
+//             });
+//     });
+// });
